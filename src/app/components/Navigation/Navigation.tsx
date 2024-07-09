17a993451c93f1212a385c4fe8fdeded05a35301
Kholov Rustam <rustamkholow5@gmail.com>
2024-07-09 17:15:05 +0500

    export default Menu;

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,38 +1,23 @@
 import Image from "next/image";
 import styles from "./Navigation.module.css";
+import { Menu } from "../Menu/Menu";
 
 export const Navigation =() => {
     return (
-    <nav className={styles.mainNav}>
-      <div className={styles.navLogo}>
-        <Image className={styles.logoImage} src="/img/logo.png" alt="Skypro-logo" width={114} height={17}/>
-      </div>
-      <div className={styles.navBurger}>
-        <span className={styles.burgerLine} />
-        <span className={styles.burgerLine} />
-        <span className={styles.burgerLine} />
-      </div>
-      <div className={styles.navMenu}>
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
-}
+      <nav className={styles.mainNav}>
+        <div className={styles.navLogo}>
+          <Image className={styles.logoImage} src="/img/logo.png" alt="Skypro-logo" width={114} height={17}/>
+        </div>
+        <div className={styles.navBurger}>
+          <span className={styles.burgerLine} />
+          <span className={styles.burgerLine} />
+          <span className={styles.burgerLine} />
+        </div>
+        <div className={styles.navMenu}>
+          <Menu/>
+        </div>
+      </nav>
+    )
+  }
 
 export default Navigation;
