d72016747fae51910c741338aafa49c85734b7b5
Kholov Rustam <rustamkholow5@gmail.com>
2024-07-09 21:21:03 +0500

    bar

--- src/app/components/Centerblock/Centerblock.tsx
+++ src/app/components/Centerblock/Centerblock.tsx
@@ -1,3 +1,4 @@
+import { PlaylistItem } from "@/app/PlaylistItem/PlaylistItem";
 import { Filter } from "../Filter/Filter";
 import PlaylistTitle from "../PlaylistTitle/PlaylistTitle";
 import Searchbar from "../Searchbar/Searchbar";
@@ -11,39 +12,10 @@ export const Centerblock =() => {
         <Filter/>
         <div className={styles.centerblockContent}>
             <PlaylistTitle/>
-            <div className="content__playlist playlist">
-                <div className="playlist__item">
-                    <div className="playlist__track track">
-                        <div className="track__title">
-                            <div className="track__title-image">
-                                <svg className="track__title-svg">
-                                    <use xlinkHref="img/icon/sprite.svg#icon-note" />
-                                </svg>
-                            </div>
-                            <div className="track__title-text">
-                                <a className="track__title-link" href="http://">
-                                Guilt <span className="track__title-span" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="track__author">
-                            <a className="track__author-link" href="http://">
-                                Nero
-                            </a>
-                        </div>
-                        <div className="track__album">
-                            <a className="track__album-link" href="http://">
-                                Welcome Reality
-                            </a>
-                        </div>
-                        <div className="track__time">
-                            <svg className="track__time-svg">
-                                <use xlinkHref="img/icon/sprite.svg#icon-like" />
-                            </svg>
-                            <span className="track__time-text">4:44</span>
-                        </div>
-                    </div>
-                </div>
+            <div className={styles.contentPlaylist}>
+                <PlaylistItem/>
+                <PlaylistItem/>
+                <PlaylistItem/>
                 {/* <div className="playlist__item">
                     <div className="playlist__track track">
                         <div className="track__title">
