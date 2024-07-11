739a0af202aba690fd61fe41467b40e200cf2fa9
Kholov Rustam <rustamkholow5@gmail.com>
2024-07-11 17:52:30 +0500

    <span className={styles.trackTitleLink}>

--- src/app/components/PlaylistItem/PlaylistItem.tsx
+++ src/app/components/PlaylistItem/PlaylistItem.tsx
@@ -1,38 +1,43 @@
+import { TrackType } from "@/types/trackstypes";
 import styles from "./PlaylistItem.module.css";
 
-export const PlaylistItem = () => {
+type TrackProps = {
+    track: TrackType
+}
+
+export const PlaylistItem = ({track}: TrackProps) => {
     return (
-    <div className={styles.playlistItem}>
-        <div className={styles.playlistTrack}>
-            <div className={styles.trackTitle}>
-                <div className={styles.trackTitleImage}>
-                    <svg className={styles.trackTitleSvg}>
-                        <use xlinkHref="img/icon/sprite.svg#icon-note" />
+        <div className={styles.playlistItem}>
+            <div className={styles.playlistTrack}>
+                <div className={styles.trackTitle}>
+                    <div className={styles.trackTitleImage}>
+                        <svg className={styles.trackTitleSvg}>
+                            <use xlinkHref="img/icon/sprite.svg#icon-note" />
+                        </svg>
+                    </div>
+                    <div className={styles.trackTitleText}>
+                        <span className={styles.trackTitleLink}>
+                        Guilt <span className={styles.trackTitleSpan} />
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.trackAuthor}>
+                    <span className={styles.trackAuthorLink}>
+                        Nero
+                    </span>
+                </div>
+                <div className={styles.trackAlbum}>
+                    <span className={styles.trackAlbumLink}>
+                        Welcome Reality
+                    </span>
+                </div>
+                <div className={styles.trackTime}>
+                    <svg className={styles.trackTimeSvg}>
+                        <use xlinkHref="img/icon/sprite.svg#icon-like" />
                     </svg>
+                    <span className={styles.trackTimeText}>4:44</span>
                 </div>
-                <div className={styles.trackTitleText}>
-                    <a className={styles.trackTitleLink} href="http://">
-                    Guilt <span className={styles.trackTitleSpan} />
-                    </a>
-                </div>
-            </div>
-            <div className={styles.trackAuthor}>
-                <a className={styles.trackAuthorLink} href="http://">
-                    Nero
-                </a>
-            </div>
-            <div className={styles.trackAlbum}>
-                <a className={styles.trackAlbumLink} href="http://">
-                    Welcome Reality
-                </a>
-            </div>
-            <div className={styles.trackTime}>
-                <svg className={styles.trackTimeSvg}>
-                    <use xlinkHref="img/icon/sprite.svg#icon-like" />
-                </svg>
-                <span className={styles.trackTimeText}>4:44</span>
             </div>
         </div>
-    </div>
     )
 }
