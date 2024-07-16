ff1ab9414ff20215962ccf394f57dda3de9e3307
Kholov Rustam <rustamkholow5@gmail.com>
2024-07-16 15:54:07 +0500

    return useContext(CurrentTrackContext)

--- src/app/components/Filter/FilterItem/FilterItem.tsx
+++ src/app/components/Filter/FilterItem/FilterItem.tsx
@@ -48,7 +48,7 @@ export function FilterItem({
                     ))}
                 </ul>
                 </div>
-                )}
+            )}
         </div>
     )
 }
--- src/contexts/CurrentTrackProvider.tsx
+++ src/contexts/CurrentTrackProvider.tsx
@@ -1,8 +1,22 @@
-import { createContext } from "react";
+import { TrackType } from "@/types/trackstypes";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
 
-// 00:14 
+type CurrentTrackContextValue = {
+    currentTrack: TrackType | null;
+    setCurrentTrack: Dispatch<SetStateAction <TrackType | null>>
+}
 
-type CurrentTrackContextValue 
+const CurrentTrackContext = createContext<CurrentTrackContextValue | null>(null)
 
-const CurrentTrackContext = createContext<>(null)
+type CurrentTrackProviderProps = {
+    children: ReactNode,
+}
 
+export function CurrentTrackProvider({children}: CurrentTrackProviderProps) {
+    const [currentTrack, setCurrentTrack] = useState<TrackType | null>(null)
+    return <CurrentTrackContext.Provider value={{currentTrack, setCurrentTrack}}>{children}</CurrentTrackContext.Provider>
+} 
+
+export function useCurrentTrack(){
+    return useContext(CurrentTrackContext)
+}
